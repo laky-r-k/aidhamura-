cabe4ae34f61acb70f4d3c5804de5cfdd655d58b
laky-r-k <lakshmishrkanchan12@gamil.com>
2025-09-30 03:41:39 +0530

    added migration

--- accounts/migrations/0003_setup_site_domain.py
+++ accounts/migrations/0003_setup_site_domain.py
@@ -9,18 +9,24 @@ def setup_site(apps, schema_editor):
     # We get your live domain name from an environment variable
     domain_name = os.environ.get('SITE_DOMAIN')
     
-    # This is the key part: update_or_create
-    # It tries to find a Site with pk=1.
-    # - If it exists, it UPDATES its domain and name.
-    # - If it does not exist, it CREATES it.
-    # This makes the migration safe to run multiple times.
-    Site.objects.update_or_create(
-        pk=1,
-        defaults={
-            'domain': domain_name,
-            'name': 'AIDHAMURA'
-        }
-    )
+    # NEW: We add a check to ensure the domain_name is not empty.
+    # This prevents the migration from crashing if the environment variable is missing.
+    if domain_name:
+        # This is the key part: update_or_create
+        # It tries to find a Site with pk=1.
+        # - If it exists, it UPDATES its domain and name.
+        # - If it does not exist, it CREATES it.
+        # This makes the migration safe to run multiple times.
+        Site.objects.update_or_create(
+            pk=1,
+            defaults={
+                'domain': domain_name,
+                'name': 'AIDHAMURA'
+            }
+        )
+    else:
+        # If the variable is not set, we print a warning and do nothing.
+        print("\nWARNING: SITE_DOMAIN environment variable not set. Skipping site setup.")
 
 
 class Migration(migrations.Migration):
