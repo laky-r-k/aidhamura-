73098ca600b98fae1497f8d622cdda683d92ad25
laky-r-k <lakshmishrkanchan12@gamil.com>
2025-10-01 00:34:27 +0530

    trail and error

--- aidhamura/settings.py
+++ aidhamura/settings.py
@@ -116,8 +116,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
