017acdd34106f86586e18ee3aff9a25c58a691f4
laky-r-k <lakshmishrkanchan12@gamil.com>
2025-09-30 21:37:50 +0530

    trying to solve login error

--- accounts/migrations/0003_setup_site_domain.py
+++ accounts/migrations/0003_setup_site_domain.py
@@ -1,40 +1,29 @@
 from django.db import migrations
 import os
 
-# This function will be run by the migration
 def setup_site(apps, schema_editor):
-    # We get the Site model from the 'sites' app
     Site = apps.get_model('sites', 'Site')
-    
-    # We get your live domain name from an environment variable
     domain_name = os.environ.get('SITE_DOMAIN')
-    
-    # NEW: We add a check to ensure the domain_name is not empty.
-    # This prevents the migration from crashing if the environment variable is missing.
+
     if domain_name:
-        # This is the key part: update_or_create
-        # It tries to find a Site with pk=1.
-        # - If it exists, it UPDATES its domain and name.
-        # - If it does not exist, it CREATES it.
-        # This makes the migration safe to run multiple times.
         Site.objects.update_or_create(
-            pk=1,
+            pk=1,  # Use pk=1 to match SITE_ID = 1
             defaults={
                 'domain': domain_name,
                 'name': 'AIDHAMURA'
             }
         )
+        print(f"Site domain set to: {domain_name}")
     else:
-        # If the variable is not set, we print a warning and do nothing.
-        print("\nWARNING: SITE_DOMAIN environment variable not set. Skipping site setup.")
+        print("\n⚠️ WARNING: SITE_DOMAIN environment variable not set. Skipping site setup.")
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        # This should be the name of your previous migration in the 'accounts' app
+        # This must point to your PREVIOUS migration in the SAME app ('accounts')
         ('accounts', '0002_create_superuser'),
-        # We also depend on the sites app's own migrations
+        # This dependency on the 'sites' app is also required
         ('sites', '0002_alter_domain_unique'),
     ]
 
